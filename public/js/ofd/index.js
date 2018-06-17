3d975783e3933cb4785a8f30f81759e17f865d51
marianna <vergunmari@gmail.com>
2018-06-17 21:17:16 +0300

    Современный слайдер Glide.js для ОФД

---
+++ public/js/ofd/index.js
@@ -0,0 +1,27 @@
+; (function () {
+
+  window.matchMedia("(min-width: 768px)").matches &&
+    new Glide('.ofd-slider', {
+      type: 'carousel', //после последнего слайда показывать первый
+      startAt: 0, //первый показываемый
+      perView: 1, //число слайдов на экране
+      // focusAt: 'center'
+      gap: 0, //отступ между слайдами
+      // peek:  { before: 100, after: 50 }, //позволяет показать часть предыдущего/следущего слайдов 
+      // autoplay: 2000, //интервал между перемоткой
+      hoverpause: true, //stop autoplay when hover
+      keyboard: true, //добавляет управление клавишами
+      direction: 'ltr', //промотка слева направо
+      breakpoints: { //изменения для разных размеров экрана
+        320: {
+          // perView: 0
+        },
+        1600: {
+          // perView: 1
+        }
+      },
+      animationDuration: 1000, //продолжительность анимации переключения на следующий слайд
+      rewindDuration: 1000, //продолжительность анимации переключения между последним и первым слайдами 
+      animationTimingFunc: 'cubic-bezier(0.165, 0.840, 0.440, 1.000)', //функция анимации 
+    }).mount()
+}());  
